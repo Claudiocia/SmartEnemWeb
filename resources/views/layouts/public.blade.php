6d5ac103522fed1658854df80c21e0d3ee0caa41
ClaudioSouza <claudiosouza.cia@gmail.com>
2019-05-11 22:49:16 -0300

    email contato2

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -38,10 +38,19 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        <li class="nav-item btn btn-primary mr-lg-3" type="button">
+                            <a class="nav-link" href="{{ route('noticias.index') }}"><span style="font-weight: bold; color: white;">{{ __('Noticias') }}</span></a>
+                        </li>
+                        <li class="nav-item btn btn-primary mr-lg-3" type="button">
+                            <a class="nav-link" href="{{ route('calendarios.index') }}"><span style="font-weight: bold; color: white;">{{ __('Calendário') }}</span></a>
+                        </li>
+                        <li class="nav-item btn btn-primary mr-lg-3" type="button">
+                            <a class="nav-link" href="{{ route('cria.email') }}"><span style="font-weight: bold; color: white;">{{ __('Contato') }}</span></a>
+                        </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ url('/') }}">{{ __('Voltar') }}</a>
-                            </li>
+                        <li class="nav-item btn btn-primary mr-lg-3" type="button">
+                            <a class="nav-link" href="{{ url('/') }}"><span style="font-weight: bold; color: white;">{{ __('Voltar') }}</span></a>
+                        </li>
 
 
                     </ul>
