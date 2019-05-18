004a05d2ffdfabc47b5a637646fae61e3e97b35c
ClaudioSouza <claudiosouza.cia@gmail.com>
2019-05-17 21:15:56 -0300

    server3

---
+++ public/server/served.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+	header("Content-Type: text/html; charset=UTF-8",true);
+
+	$f = fopen('POST_DATA.txt', 'a');
+	fwrite($f, 'Mensagem: '.UTF8_decode($_POST['local'])."\r\n\r\n");
+	fwrite($f, 'Data/Hora: '.$_POST['data']."\r\n\r\n");
+	fwrite($f, 'Header: '.$_POST['header']."\r\n\r\n");
+	fclose($f);
+	
+	echo 'Mensagem que veio do site recebida com sucesso<br/>';
+	echo 'mensagem: '.UTF8_decode($_POST['local']);
+?>
